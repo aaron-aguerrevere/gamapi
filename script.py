17f01f68d46d5e5e104605e307ef793f5bb8a08f
aaron a <aaguerrevere@legacy.com>
2021-06-18 15:17:47 -0500

    was able to connect to the API using auth2_client, retreiving object for now

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
-############------------ IMPORTS ------------############
+from googleads import ad_manager
+from googleads import oauth2
 
-############------------ GLOBAL VARIABLES ------------############
+key_file = 'C:/Users/aaguerrevere/gamapi-317120-99b0a97326bc.json'
 
-############------------ FUNCTIONS ------------############
+# Initialize the GoogleRefreshTokenClient using the credentials you received
+# in the earlier steps.
+oauth2_client = oauth2.GoogleServiceAccountClient(
+    key_file, oauth2.GetAPIScope('ad_manager'))
 
-############------------ TESTS ------------############
+# Initialize the Ad Manager client.
+ad_manager_client = ad_manager.AdManagerClient(oauth2_client, 'gamapi_app')
+print(ad_manager_client)
 
-############------------ DRIVER CODE ------------############
